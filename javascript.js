2290165c5bab8d86c319d75b2a07905f0413a919
AllyciaP <140102579+AllyciaP@users.noreply.github.com>
2024-10-24 15:13:38 -0400

    Add function and instructions to play 1 game round
    created and added playRound function, which uses the selections of the
    player and computer to determine winner or tie; also added instructions
    to play 1 round and give score update

--- javascript.js
+++ javascript.js
@@ -2,16 +2,16 @@
 // this function selects choice for computer//
 function getComputerChoice() {
 
-    //the choice is declared, which will hold the string rock, paper or scissors//
+    //the choice is declared, which will hold the string rock, paper or scissors
     let choice = "";
 
-    //declares varibale num and holds a random number //
+    //declares varibale num and holds a random number 
     let num = Math.random();
 
-    // DELETE for debugging //
+    // DELETE for debugging 
     console.log(num);
 
-    //choice depends on  number in num variable //
+    //choice depends on  number in num variable 
     if (num >= 0 && num < 0.333) {
         choice = "rock";
     }
@@ -28,12 +28,67 @@ function getComputerChoice() {
     return choice;
 }
 
+// this function prompts for player to enter choice, which is then returned 
 function getHumanChoice() {
-    let humanChoice = prompt("Choose - Rock, Paper, or Scissors: ");
+    let humanChoice = prompt("Choose - rock, paper, or scissors: ");
 
-    console.log(humanChoice);    
+    // DELETE
+    console.log(humanChoice);
 
     return humanChoice;
+}
+
+// this function, using player's and computer's choice,
+// allows ONE round of game, checking who beats who, then returning the winner
+function playRound(humanChoice, computerChoice) {
+
+    let hChoice = humanChoice.toLowerCase();
+    let winner = "";
+
+    if (computerChoice === "rock") {
+        if (hChoice === "paper") {
+            console.log("You Win! Paper beats Rock!");
+            winner = "player";
+            return winner;
+        } else if (hChoice === "scissors") {
+            console.log("You Lose! Rock beats Scissors!");
+            winner = "computer";
+            return winner;
+        } else if (hChoice === "rock") {
+            console.log("Tie - NO points!");
+            winner = "none";
+            return winner;
+        }
+    } else if (computerChoice === "paper") {
+        if (hChoice === "rock") {
+            console.log("You Lose! Paper beats Rock!");
+            winner = "computer";
+            return winner;
+        } else if (hChoice === "scissors") {
+            console.log("You Win! Scissors beat Paper!");
+            winner = "player";
+            return winner;
+        } else if (hChoice === "paper") {
+            console.log("Tie - No points!");
+            winner = "none";
+            return winner;
+        }
+    } else if (computerChoice === "scissors") {
+        if (hChoice === "rock") {
+            console.log("You Win! Rock beats Scissors!");
+            winner = "player";
+            return winner;
+        } else if (hChoice === "paper") {
+            console.log("You lose! Scissors beat Paper!");
+            winner = "computer";
+            return winner;
+        } else if (hChoice === "scissors") {
+            console.log("Tie - No Points!");
+            winner = "none";
+            return winner;
+        }
+    }
+
 }
 
 //function is called and value returned is held in compChoice 
@@ -42,6 +97,22 @@ let compChoice = getComputerChoice();
 // DELETE:: prints value held in compChoice 
 console.log("return of function held in compchoice is --> " + compChoice);
 
+//
 let humChoice = getHumanChoice();
 
+// DELETE 
 console.log("human choice is -->" + humChoice);
+
+
+winnerGame = playRound(humChoice, compChoice);
+
+let humanScore = 0;
+let computerScore = 0;
+
+if (winnerGame === "player") {
+    humanScore = humanScore + 1;
+    console.log("player score = " + humanScore + "----> computer score = " + computerScore);
+} else if (winnerGame === "computer") {
+    computerScore = computerScore + 1; 
+    console.log("player score = " + humanScore + "-----> computer score = " + computerScore);
+}
